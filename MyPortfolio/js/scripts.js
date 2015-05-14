1fa177fe45a8effbc8685a06ae816a6b065c4627
almazka <almazka@flylady.su>
2015-05-14 12:50:47 +0600

    end layout of portfolio

--- MyPortfolio/js/scripts.js
+++ MyPortfolio/js/scripts.js
@@ -1,107 +1,42 @@
 $(document).ready(function(){
 	
-		/*colorbox*/
-		if($('a.stage-lnk.zoom').length > 0){
-		$("a.stage-lnk.zoom").colorbox();
-		}
-		/*end colorbox*/
-
-		/* filter image block */
-	
-		if($('#filter').length > 0){
-		var items = $('#stage li'),
-			itemsByTags = {};
-
-		// Looping though all the li items:
-
-		items.each(function(i){
-			var elem = $(this),
-				tags = elem.data('tags').split(',');
-			
-			// Adding a data-id attribute. Required by the Quicksand plugin:
-			elem.attr('data-id',i);
-			
-			$.each(tags,function(key,value){
-				
-				// Removing extra whitespace:
-				value = $.trim(value);
-				
-				if(!(value in itemsByTags)){
-					// Create an empty array to hold this item:
-					itemsByTags[value] = [];
-				}
-				
-				// Each item is added to one array per tag:
-				itemsByTags[value].push(elem);
-			});
-			
+		/* prettyPhoto */
+		
+		if($("a[rel^='prettyPhoto']").length > 0){
+			$("a[rel^='prettyPhoto']").prettyPhoto({
+			animation_speed: 'fast', /* скорость анимации про загрузке и смене фото, значение fast, slow или normal */
+			autoplay_slideshow: true, /* разрешить слайд шоу, значение true или false */
+			slideshow: 3000, /* false или интервал в миллисекундах (работает если autoplay_slideshow: true)*/
+			opacity: 0.60, /* Сила затемнения, (допустимые значения от 0.1 до 1) 0.1 - самое слабое, 1 - самое сильное */
+			show_title: true, /* Показывает наименование товара, значение true или false */
+			default_width: 500,
+			autoplay_slideshow: false,
+			default_height: 500,
+			counter_separator_label: ' из ', /* разделитель для счётчика, по умолчанию косая черта (слэш) "/" */
+			theme: 'facebook', /* указываем тему: light_rounded, dark_rounded, light_square, dark_square или facebook */
+			modal: false, /* если установлено значение True, закрыть окно можно только по нажатию "Закрыть" */
+			social_tools: false
 		});
-
-		// Creating the "All" option in the menu:
-		createList('Все',items);
-
-		// Looping though the arrays in itemsByTags:
-		$.each(itemsByTags,function(k,v){
-			createList(k,v);
-		});
-
-		$('#filter a').on('click',function(e){
-			var link = $(this).parent();
-			
-			link.addClass('active').siblings().removeClass('active');
-			
-			// Using the Quicksand plugin to animate the li items.
-			// It uses data('list') defined by our createList function:
-			
-			$('#stage').quicksand(link.data('list').find('li'));
-			e.preventDefault();
-		});
-
-		$('#filter a:first').click();
-
-		function createList(text,items){
-			
-			// This is a helper function that takes the
-			// text of a menu button and array of li items
-			
-			// Creating an empty unordered list:
-			var ul = $('<ul>',{'class':'hidden'});
-			
-			$.each(items,function(){
-				// Creating a copy of each li item
-				// and adding it to the list:
-				
-				$(this).clone().appendTo(ul);
-			});
-
-			ul.appendTo('#container');
-
-			// Creating a menu item. The unordered list is added
-			// as a data parameter (available via .data('list'):
-			var lii = $('<li>', {
-				data: {list:ul}
-			});
-			var a = $('<a>',{
-				html: text,
-				href:'#',
-			});
-			a.appendTo(lii);
-			lii.appendTo('#filter');
-		}
 		}
 		
+		/* end of prettyPhoto */
+
+		/* filter image block */
+		if($('#container').length > 0){
+			$('#container').mixItUp();
+		}
 		/* end of filter image block */
 
 
 		/* ancor */
 
 		$('a[href*=#]').bind("click", function(e){
-		     var anchor = $(this);
-		     $('html, body').stop().animate({
-		        scrollTop: $(anchor.attr('href')).offset().top-50
-		     }, 1000);
-		     e.preventDefault();
-		  });
+				 var anchor = $(this);
+				 $('html, body').stop().animate({
+						scrollTop: $(anchor.attr('href')).offset().top-50
+				 }, 1000);
+				 e.preventDefault();
+			});
 		
 		/* end of ancor */
 
--- MyPortfolio/js/toscripts.js
+++
@@ -1,5 +0,0 @@
-/*colorbox*/
-if($('.colorbox-item').length > 0){
-$(".colorbox-item").colorbox();
-}
-/*end colorbox*/
